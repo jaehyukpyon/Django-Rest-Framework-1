ee651b2ee04c1dbbbf3bf818bb78eb2a713076ee
jaehyuk <jaehyuk.pyon@gmail.com>
2023-01-19 10:09:28 +0900

    product/views.py의 get_queryset method에 querystring 적용

--- product/views.py
+++ product/views.py
@@ -13,7 +13,25 @@ class ProductListView(
     serializer_class = ProductSerializer
     
     def get_queryset(self):
-        return Product.objects.all().order_by('id')    
+        # name = self.request.query_params.get('name')
+        # products = Product.objects.all()
+        # if name:
+        #   products = products.filter(name__contains=name)
+        
+        products = Product.objects.all() # 여러 개의 결과값을 가져오는 QuerySet!!! 이 순간 DB에서 가져오지 않음
+        
+        if 'price' in self.request.query_params:
+            price = self.request.query_params['price']
+            products = products.filter(price__lte=price) # 이 순간 DB에서 가져오지 않음
+            
+        if 'name' in self.request.query_params:
+            name = self.request.query_params['name']
+            products = products.filter(name__contains=name) # 이 순간 DB에서 가져오지 않음
+        
+            
+        products = products.order_by('id') # 이 순간 DB에서 가져오지 않음
+        
+        return products    
     
     def get(self, request, *args, **kwargs):
         # Queryset
