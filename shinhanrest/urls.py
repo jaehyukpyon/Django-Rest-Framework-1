46ed58622aef37a2022fdaa4d8904b608f652199
jaehyuk <jaehyuk.pyon@gmail.com>
2023-01-26 11:00:17 +0900

    수업시간

--- shinhanrest/urls.py
+++ shinhanrest/urls.py
@@ -25,7 +25,7 @@ from rest_framework_simplejwt.views import (
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/product', include('product.urls')), # api/product로 시작하는 애는 include('product.urls')로 보내라
-    path('api/token', TokenObtainPairView.as_view()),
-    path('api/member', include('member.urls')),
+    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/member/', include('member.urls')),
     path('__debug__/', include('debug_toolbar.urls')),
 ]
